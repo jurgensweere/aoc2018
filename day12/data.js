ba4a684c20e0003c867ddf76a96a30ca724ec625
Jurgen Sweere <jurgen@MacBook-Pro-van-Jurgen.local>
2018-12-18 21:14:52 +0100

    Parse day 12 data

---
+++ day12/data.js
@@ -0,0 +1,18 @@
+let input = require('../shared/input')(__dirname)
+    .split('\n');
+
+let initialState = /[\.\#]+/.exec(input[0])[0];
+// initialState = initialState.replace(/#/g, '1')
+// initialState = initialState.replace(/\./g, '0').split('').map(v => parseInt(v));
+const notes = input.slice(2).map((v) => {
+    let data = /(.*)\ =>\ (.*)/.exec(v)
+    return {
+        pattern: data[1],
+        next: data[2]
+    }
+});
+
+module.exports = {
+    state: initialState,
+    notes
+};
